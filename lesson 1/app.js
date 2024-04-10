a9751325b0d5926b2fd8bd6eba2a74b4f8caa1c6
hashim <hashim1010@gmail.com>
2024-04-10 21:42:23 +0530

    episode 1 done

--- lesson 1/app.js
+++ lesson 1/app.js
@@ -1,4 +1,28 @@
-const heading = React.createElement("h1",{heading},"hello world from react");
+//nexted html elements
+/*
+<div id="parent">
+    <div id ="child">
+        <h1></h1>
+    </div>
+
+</div>
+*/
+
+const parent = React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"im an h1 tag"),
+        React.createElement("h2",{},"im an h2 tag")]
+
+    ),React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"im an h1 tag"),
+        React.createElement("h2",{},"im an h2 tag")]
+
+    )]
+);
+
+// const heading = React.createElement("h1",{id:"heading"},"hello world from react");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+
+console.log(parent);  // returns an object 
+root.render(parent); // take the object and convert into h1 tag and render it to the root.
