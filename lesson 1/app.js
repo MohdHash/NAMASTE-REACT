5be2720b6a5c598f6fc64e99643569a9c400f74b
hashim <hashim1010@gmail.com>
2024-04-15 02:06:06 +0530

    Do lesson04 part 1

--- lesson 1/app.js
+++ lesson 1/app.js
@@ -3,32 +3,99 @@ import React from 'react';
 
 import ReactDOM from "react-dom/client";
 
-// React Element => Object
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 
-// JSX => React.createElement => ReactElement - JS object => HTMLelement(render)
-// React element
-// const jsxheading = <h1 id='heading'>HashimFazeela </h1>;
 
-//React componSent 
-//class based component (old)
-// functional components (new)
-const Small = () => (
-   <h1>
-    Namaste React
-   </h1>
-);
 
-//component composition
-const HeadingComponent = () => (
-    <div id="container">
-        <Small />  
-        <h1 id="heading">HashimFazeela</h1>
-    </div>
-);
 
+/**
+ * 
+ * Header
+ *  -Logo
+ *  -Nav items 
+ * Body 
+ *  -search 
+ *  -Restaurant container 
+ *     -RestaurantCards 
+ * Footer 
+ *  -Copyright
+ *  -address
+ *  -contacts 
+ *  -Links
+ * 
+ */
+const Header = ()=>{
+    return (
+        <div className="header">
+            <div className="img-container">
+                <img src='https://i.pinimg.com/originals/34/0c/6a/340c6add7519212185a08d4205eb1965.png'>
+                </img>
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>About</a></li>
+                    <li><a href='#'>Contact</a></li>
+                    <li><img src='https://i.pinimg.com/736x/d6/39/07/d6390726609aa16dddbb604a7d5afe19.jpg'></img></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = ()=>{
+    return(
+        <div className='res-card'>
+            <img 
+                className='res-logo'
+                alt='res-logo'
+                src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/1/9c56708a-5fd3-479e-8bad-563e60c3cc79_140880.jpg'
+            ></img>
+            <h3>MacDonald's</h3>
+            <span><FontAwesomeIcon icon="fa-regular fa-star" /></span>
+            <span>4.3</span>
+            <span>25-30 mins</span>      
+        </div>
+    )
+}
+
+const Body = ()=>{
+    return(
+        <div className='body-container'>
+            <div className='searchBar'>Search</div>
+            <div className='res-container'>
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+
+
+            </div>
+        </div>
+
+    )
+}
+
+const AppLayout = ()=>{
+    return (
+        <div className="body">
+            <Header />
+            <Body />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
+
+
 
